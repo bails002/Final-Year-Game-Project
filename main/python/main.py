b13d7149f180bd097db0d6bed2482474681368f3
bailey caplan <zkac236@live.rhul.ac.uk>
2023-11-03 23:04:38 +0000

    window added and size params decided

--- main/python/main.py
+++ main/python/main.py
@@ -1,1 +1,15 @@
 import pygame
+import sys
+
+pygame.init()
+pygame.font.init()
+myfont = pygame.font.SysFont('Comic Sans MS', 5)
+display_width = 800
+display_height = 600
+# create screen
+screen = pygame.display.set_mode((display_width, display_height))
+
+while True:
+    pygame.display.set_caption("adventure game")
+    window = screen
+    window.fill((255,255,255))
